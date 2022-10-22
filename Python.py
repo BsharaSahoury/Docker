6505cea3f3750c6e41587d7463ecb8f55999db58
BSHARA <57354656+BsharaSahoury@users.noreply.github.com>
2022-10-23 00:42:41 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -4,7 +4,7 @@ start_time = time.time()
 with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
     f.write('Python.py file output\n')
 
-time.sleep(5)
+time.sleep(10)
 
 if (time.time() - start_time) >= 10:
     with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
