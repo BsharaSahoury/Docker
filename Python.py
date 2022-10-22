b93371cd5ab9fde45024d8fb28d81d7bbc52d460
BSHARA <57354656+BsharaSahoury@users.noreply.github.com>
2022-10-23 00:38:48 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -4,6 +4,8 @@ start_time = time.time()
 with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
     f.write('Python.py file output\n')
 
+time.sleep(15)
+
 if (time.time() - start_time) >= 10:
     with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
         f.write('Failed test')
