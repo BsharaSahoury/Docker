21a51337d927b7238b5fc70ec9ce713c8f656534
BSHARA <57354656+BsharaSahoury@users.noreply.github.com>
2022-10-23 19:26:34 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python 
 #test.py
+import sys
 
 import multiprocessing
 import time
@@ -9,7 +10,7 @@ start_time = time.time()
 def foo():
     with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
     f.write('Python.py file output\n')
-    exit()
+    sys.exit()
 
 if __name__ == '__main__':
     # Start foo as a process
