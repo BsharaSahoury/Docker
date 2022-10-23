80a20d22f15800a287b362054954b688caa636a4
BSHARA <57354656+BsharaSahoury@users.noreply.github.com>
2022-10-23 19:21:58 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,12 +1,29 @@
+#!/usr/bin/env python 
+#test.py
+
+import multiprocessing
 import time
+
 start_time = time.time()
 
-with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
+def foo():
+    with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
     f.write('Python.py file output\n')
+    exit()
 
-time.sleep(10)
+if __name__ == '__main__':
+    # Start foo as a process
+    p = multiprocessing.Process(target=foo, name="Foo")
+    p.start()
+     
+    # Wait 10 seconds for foo
+    time.sleep(10)
+    
+    # Terminate foo
+    p.terminate()
 
-if (time.time() - start_time) >= 10:
+    # Cleanup
+    p.join()
+    
     with open('/home/Docker-proj/volume/Output.txt', 'w') as f:
         f.write('Failed test\n')
-    
